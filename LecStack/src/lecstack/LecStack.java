70e7fbadd2fedb79fdc0e027383389e1c9eae5b3
I <piligrim.a3@gmail.com>
2021-02-01 10:21:29 +0900

    add: client code

--- LecStack/src/lecstack/LecStack.java
+++ LecStack/src/lecstack/LecStack.java
@@ -7,6 +7,15 @@ public class LecStack {
 
 
     public static void main(String[] args) {
+        LStack stack = new LStack(10);
+        
+        for(int i=0; i<10; i++) {
+            stack.push(i);
+        }
+        
+        while (!stack.isEmpty()) {            
+            System.out.println(stack.pop());
+        }
         
     }
     
